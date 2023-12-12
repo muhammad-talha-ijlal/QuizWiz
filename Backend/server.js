172205a515e49096cb3af88ec7aa742ac2ea92c5
Muhammad Abdullah <mafivs51@gmail.com>
2023-12-12 20:11:27 +0500

    changes in server.js
    connected backend of questions

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const userRoutes = require("./routes/userRoute");
 const classRoutes = require("./routes/classRoute");
 const quizRoutes = require("./routes/quizRoute");
+const questionRoutes = require("./routes/questionRoute");
 require("./utils/db");
 
 const app = express();
@@ -15,6 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api", userRoutes);
 app.use("/api", classRoutes);
 app.use("/api", quizRoutes);
+app.use("/api", questionRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
